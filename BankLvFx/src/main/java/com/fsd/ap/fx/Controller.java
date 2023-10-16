a9a7e2cbd00856402df7aa892c4394c93abb4d36
Georges034302 <georges.boughantous@gmail.com>
2023-10-16 19:31:40 +1100

    lab10 deleted

--- BankLvFx/src/main/java/com/fsd/ap/fx/Controller.java
+++
@@ -1,8 +0,0 @@
-package com.fsd.ap.fx;
-
-import javafx.stage.*;
-
-public abstract class Controller<M> {
-	protected M model;
-	protected Stage stage;
-}
--- BankLvFx/src/main/java/com/fsd/model/InsufficientFundsException.java
+++
@@ -1,7 +0,0 @@
-package com.fsd.model;
-
-public class InsufficientFundsException extends Exception {
-	public InsufficientFundsException() {
-		super("Insufficient funds");
-	}
-}
